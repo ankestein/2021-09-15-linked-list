73b32f779d2edbe1ac2e9a69c424802e787bf7f6
Anke <a.stein@posteo.de>
2021-09-15 10:20:36 +0200

    Create AnimalListItem

---
+++ src/main/java/model/AnimalListItem.java
@@ -0,0 +1,11 @@
+package model;
+
+public class AnimalListItem {
+    private Animal value;
+    private AnimalListItem next;
+
+    // costructor
+    public AnimalListItem(Animal value) {
+        this.value = value;
+        }
+}
