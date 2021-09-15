c5e054ffbe0257cae1a3ff014b9bbac05837f077
Anke <a.stein@posteo.de>
2021-09-15 10:29:22 +0200

    Add getter and setter

--- src/main/java/model/AnimalListItem.java
+++ src/main/java/model/AnimalListItem.java
@@ -4,8 +4,25 @@ public class AnimalListItem {
     private Animal value;
     private AnimalListItem next;
 
-    // costructor
+    // constructor
     public AnimalListItem(Animal value) {
         this.value = value;
         }
+
+    // getter and setter
+    public Animal getValue(){
+        return value;
+    }
+
+    public void Animal setValue(Animal value){
+        this.value = value;
+    }
+
+    public AnimalListItem getNext() {
+        return next;
+    }
+
+    public void setNext(AnimalListItem next) {
+        this.next = next;
+    }
 }
